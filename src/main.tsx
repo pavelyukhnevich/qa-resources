d5f33805e4c86c8511b660f4ea28016a3f40a887
Pavel Yukhnevich <pavel.yukhnevich@LVG-9256.local>
2025-02-04 15:57:45 +0000

    Add basename for routing

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
+const basename = "/qa-resources";
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={basename}>
 			<App />
 		</BrowserRouter>
 	</StrictMode>
